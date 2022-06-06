8eb0d8d8ddc1f9feabe9eb83e8c5606fbf15e065
relt <haxrelt@gmail.com>
2022-06-06 23:38:03 +0300

    Архетиктура приложения

---
+++ CV19/Views/Windows/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace CV19
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
